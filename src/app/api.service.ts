714c2521bdfa266189191b336668c8e1d07b8262
vaseem <vaseem@navedas.com>
2020-08-16 19:24:58 +0530

    first commit

---
+++ src/app/api.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  baseurl="http://127.0.0.1:8000"
+  httpHeaders=new HttpHeaders({'Content-type': 'application/json'})
+  constructor(private http: HttpClient) { }
+  getAllUsers(): Observable <any>{
+    return this.http.get(this.baseurl+'/api/',{headers:this.httpHeaders})
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { ApiService } from './api.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[ApiService]
 })
 export class AppComponent {
-  title = 'crudangular';
+  // title = 'crudangular';
+  users=[{title:'test'}]
+  constructor (private api:ApiService){
+    this.getUsers();
+  }
+    getUsers=()=>{
+      this.api.getAllUsers().subscribe(
+          data => {
+            this.users=data
+            console.log("---------------->",data)
+          },
+          error => {
+            console.log(error)
+          }
+      )
+    }
+  
 }
